d54796e26ab7419a57b2003255b6a8e3a37f17c3
Vipul Soni <148994074+sonivipul26@users.noreply.github.com>
2026-06-26 00:19:59 +0530

    Time: 1 ms (60.28%) | Memory: 43 MB (82.67%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            char ch=stones.charAt(i);
+            for(int j=0;j<jewels.length();j++){
+                if(ch==jewels.charAt(j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
